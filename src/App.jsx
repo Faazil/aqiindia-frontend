d5ec955c2457c2f021cd842010ff7e9034363800
Faazil-uddin-007 <fazil434@gmail.com>
2025-12-01 00:54:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import City from "./pages/City";
+import Privacy from "./pages/info/Privacy";
+import Terms from "./pages/info/Terms";
+import About from "./pages/info/About";
+import Contact from "./pages/info/Contact";
 
 export default function App(){
   return (
@@ -8,6 +12,10 @@ export default function App(){
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/city/:slug" element={<City/>} />
+        <Route path="/privacy" element={<Privacy/>} />
+        <Route path="/terms" element={<Terms/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/>} />
       </Routes>
     </BrowserRouter>
   )
