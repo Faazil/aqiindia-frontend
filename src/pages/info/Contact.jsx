102c4fbc13a3728f4c501eca566b8c5534be95d2
Faazil-uddin-007 <fazil434@gmail.com>
2025-12-01 00:52:19 +0530

    Create Contact.jsx

---
+++ src/pages/info/Contact.jsx
@@ -0,0 +1,8 @@
+export default function Contact(){
+  return (
+    <div className="container">
+      <h1>Contact</h1>
+      <p>For business/school reports and support: <a href="mailto:contact@aqiindia.live">contact@aqiindia.live</a></p>
+    </div>
+  )
+}
