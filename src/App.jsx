ca863363ba5eed0fa9fecadc086f321f2ff128cd
Faazil-uddin-007 <fazil434@gmail.com>
2025-12-01 03:04:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import City from "./pages/City";
+import CityPage from "./pages/city/slug";
 
-// city pages
-import Delhi from "./pages/cities/Delhi";
-import Mumbai from "./pages/cities/Mumbai";
-import Bengaluru from "./pages/cities/Bengaluru";
-import Hyderabad from "./pages/cities/Hyderabad";
-import Pune from "./pages/cities/Pune";
-import Kolkata from "./pages/cities/Kolkata";
-import Chennai from "./pages/cities/Chennai";
-import Ahmedabad from "./pages/cities/Ahmedabad";
-import Lucknow from "./pages/cities/Lucknow";
-import Jaipur from "./pages/cities/Jaipur";
-
-export default function App(){
+export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>} />
-        {/* dynamic route (if you want to keep it) */}
-        <Route path="/city/:slug" element={<City/>} />
-
-        {/* SEO static city pages */}
-        <Route path="/delhi" element={<Delhi/>} />
-        <Route path="/mumbai" element={<Mumbai/>} />
-        <Route path="/bengaluru" element={<Bengaluru/>} />
-        <Route path="/hyderabad" element={<Hyderabad/>} />
-        <Route path="/pune" element={<Pune/>} />
-        <Route path="/kolkata" element={<Kolkata/>} />
-        <Route path="/chennai" element={<Chennai/>} />
-        <Route path="/ahmedabad" element={<Ahmedabad/>} />
-        <Route path="/lucknow" element={<Lucknow/>} />
-        <Route path="/jaipur" element={<Jaipur/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/city/:slug" element={<CityPage />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
